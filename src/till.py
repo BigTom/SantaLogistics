bfcf4273cd777e73b4908d8fd1d0d93333c45258
Tom <tom.ayerst@gmail.com>
2020-12-18 18:05:08 +0000

    code in session

--- src/till.py
+++ src/till.py
@@ -1,2 +1,14 @@
 def echo(name):
     return "{0} {1}".format(name, name)
+
+
+def read_sack_contents(path_to_data_file):
+    print(path_to_data_file)
+    with open(path_to_data_file, "r") as req:
+        lines = [line.strip().split(",") for line in req]
+
+    item_dict = { item[0]: int(item[1]) for item in lines [2:]}
+    return item_dict
+
+def calculate_item_total(count, price):
+    return { k:v * price[k] for k,v in count }
--- tests/test_till.py
+++ tests/test_till.py
@@ -7,3 +7,40 @@ class Test(TestCase):
         expected = "tests tests"
         actual = till.echo("tests")
         self.assertEqual(expected, actual)
+
+    def test_read_the_sack_items(self):
+        expected = {"TOP": 10,
+                    "HOOP": 40,
+                    "HULA HOOP": 20}
+        actual = till.read_sack_contents("../data/example_sack.csv")
+        self.assertEqual(expected, actual)
+
+#This test intends to show multiplying a count of item * the price of the item
+# The implementation doesn't work and we didn't dig into why
+    def test_item_count(self):
+        # item_and_counts = till.read_sack_contents("../data/example_sack.csv")
+        item_and_counts = {"TOP": 10, "HOOP": 40, "HULA HOOP": 20}
+        prices_fake = {"TOP": 2.0, "HOOP": 3.5, "HULA HOOP": 5.25}
+
+        actual = till.calculate_item_total(item_and_counts, prices_fake)
+        expected = {"TOP": 20,
+                    "HOOP": 140,
+                    "HULA HOOP": 105}
+
+        self.assertEqual(expected, actual)
+
+#     def test_print_invoice(self):
+#         expected = """
+# WORKSHOP NAME
+# GIFT COUNT Total Cost
+# TOP 10 20.00
+# HOOP 40 140.00
+# HULA HOOP 20 105.00
+#
+# Total: 265.00
+# Discount: 7.5%
+# Total: 245.50
+# """
+#         # actual = till.print_invoice("../data/example_sack.csv")
+#         actual = ""
+#         self.assertEqual(expected, actual)
