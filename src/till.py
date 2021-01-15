5a4d325afc147a3e17e5584d663f0ee6300ffb0d
Tom <tom.ayerst@gmail.com>
2021-01-15 15:37:56 +0000

    8th Jan Session

--- src/till.py
+++ src/till.py
@@ -1,14 +1,32 @@
-def echo(name):
-    return "{0} {1}".format(name, name)
-
-
-def read_sack_contents(path_to_data_file):
-    print(path_to_data_file)
+def read_sack_request_contents(path_to_data_file):
     with open(path_to_data_file, "r") as req:
         lines = [line.strip().split(",") for line in req]
 
-    item_dict = { item[0]: int(item[1]) for item in lines [2:]}
-    return item_dict
+    item_dict = {item[0]: int(item[1]) for item in lines[2:]}
+    return {
+        "workshop": lines[0][0],
+        "items": item_dict}
 
-def calculate_item_total(count, price):
-    return { k:v * price[k] for k,v in count }
+
+def calculate_item_total(request, price):
+    return {product: count * price[product] for product, count in request.items()}
+
+
+def read_gift_prices(path_to_data_file, workshop_name):
+    with open(path_to_data_file, "r") as req:
+        lines = [line.strip().split(",") for line in req]
+    filter_by_workshop = {item[0].strip(): float(item[2][1:]) for item in lines[1:] if
+                          len(item) > 1 and item[1] == workshop_name}
+    return filter_by_workshop
+
+
+def calculate_net_invoice_price(total_row_costs):
+    return sum(total_row_costs.values())
+
+
+def create_invoice_content(sack_request_file, param1):
+    sack_request_content = read_sack_request_contents(sack_request_file)
+
+    return {'items': [['TOP', 10, 20], ['HOOP', 40, 140], ['HULA HOOP', 20, 105]],
+            'net_total': "FIX ME NEXT",
+            'workshop': sack_request_content["workshop"]}
--- tests/test_till.py
+++ tests/test_till.py
@@ -3,22 +3,24 @@ import till
 
 
 class Test(TestCase):
-    def test_echo(self):
-        expected = "tests tests"
-        actual = till.echo("tests")
-        self.assertEqual(expected, actual)
-
     def test_read_the_sack_items(self):
-        expected = {"TOP": 10,
-                    "HOOP": 40,
-                    "HULA HOOP": 20}
-        actual = till.read_sack_contents("../data/example_sack.csv")
+        expected = {"workshop": "ELVES",
+                    "items": {"TOP": 10,
+                              "HOOP": 40,
+                              "HULA HOOP": 20}}
+        actual = till.read_sack_request_contents("../data/example_sack.csv")
         self.assertEqual(expected, actual)
 
-#This test intends to show multiplying a count of item * the price of the item
-# The implementation doesn't work and we didn't dig into why
-    def test_item_count(self):
-        # item_and_counts = till.read_sack_contents("../data/example_sack.csv")
+    def test_read_the_gift_prices(self):
+        expected = {"TOP": 2.00,
+                    "HOOP": 3.50,
+                    "DOLL": 4.65,
+                    "BLOCKS": 12.00,
+                    "HULA HOOP": 5.25}
+        actual = till.read_gift_prices("../data/gifts.csv", "ELVES")
+        self.assertEqual(expected, actual)
+
+    def test_item_totals_are_calculated_from_product_and_counts(self):
         item_and_counts = {"TOP": 10, "HOOP": 40, "HULA HOOP": 20}
         prices_fake = {"TOP": 2.0, "HOOP": 3.5, "HULA HOOP": 5.25}
 
@@ -29,18 +31,19 @@ class Test(TestCase):
 
         self.assertEqual(expected, actual)
 
-#     def test_print_invoice(self):
-#         expected = """
-# WORKSHOP NAME
-# GIFT COUNT Total Cost
-# TOP 10 20.00
-# HOOP 40 140.00
-# HULA HOOP 20 105.00
-#
-# Total: 265.00
-# Discount: 7.5%
-# Total: 245.50
-# """
-#         # actual = till.print_invoice("../data/example_sack.csv")
-#         actual = ""
-#         self.assertEqual(expected, actual)
+    def test_calculate_net_invoice_price(self):
+        total_row_costs = {"TOP": 20,
+                           "HOOP": 140,
+                           "HULA HOOP": 105}
+        actual = till.calculate_net_invoice_price(total_row_costs)
+        expected = 265
+        self.assertEqual(expected, actual)
+
+    def test_create_invoice_content(self):
+        actual = till.create_invoice_content("../data/example_sack.csv","../data/gifts.csv" )
+        expected = {"workshop": "ELVES",
+                    "items": [["TOP", 10,20],
+                              ["HOOP", 40, 140],
+                              ["HULA HOOP", 20, 105]],
+                    "net_total": 265}
+        self.assertEqual(expected, actual)
